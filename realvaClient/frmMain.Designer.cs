bcaa3c59f40ae3f80b184666a50e28f6bc053d7a
nemanja <nemanja>
2020-04-16 21:56:32 +0200

    initial

---
+++ realvaClient/frmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace realvaClient
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnToggleConnection = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.lblConnectionStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.txtAircraft = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFlight = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecretCode = new System.Windows.Forms.TextBox();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnToggleConnection
+            // 
+            this.btnToggleConnection.Location = new System.Drawing.Point(12, 12);
+            this.btnToggleConnection.Name = "btnToggleConnection";
+            this.btnToggleConnection.Size = new System.Drawing.Size(134, 36);
+            this.btnToggleConnection.TabIndex = 0;
+            this.btnToggleConnection.Text = "Connect/Disconnect";
+            this.btnToggleConnection.UseVisualStyleBackColor = true;
+            this.btnToggleConnection.Click += new System.EventHandler(this.btnToggleConnection_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblConnectionStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 167);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(329, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(104, 17);
+            this.lblConnectionStatus.Text = "Connection Status";
+            // 
+            // timerMain
+            // 
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // txtAircraft
+            // 
+            this.txtAircraft.Location = new System.Drawing.Point(234, 21);
+            this.txtAircraft.Name = "txtAircraft";
+            this.txtAircraft.Size = new System.Drawing.Size(88, 20);
+            this.txtAircraft.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Aircraft";
+            // 
+            // btnFlight
+            // 
+            this.btnFlight.Location = new System.Drawing.Point(94, 108);
+            this.btnFlight.Name = "btnFlight";
+            this.btnFlight.Size = new System.Drawing.Size(134, 40);
+            this.btnFlight.TabIndex = 4;
+            this.btnFlight.Text = "Start/End flight";
+            this.btnFlight.UseVisualStyleBackColor = true;
+            this.btnFlight.Click += new System.EventHandler(this.btnFlight_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Secret code";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtSecretCode
+            // 
+            this.txtSecretCode.Location = new System.Drawing.Point(89, 63);
+            this.txtSecretCode.Name = "txtSecretCode";
+            this.txtSecretCode.Size = new System.Drawing.Size(233, 20);
+            this.txtSecretCode.TabIndex = 5;
+            this.txtSecretCode.TextChanged += new System.EventHandler(this.txtSecretCode_TextChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 189);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSecretCode);
+            this.Controls.Add(this.btnFlight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAircraft);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.btnToggleConnection);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Realva client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToggleConnection;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblConnectionStatus;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.TextBox txtAircraft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFlight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecretCode;
+    }
+}
+
---
+++ realvaClient/frmMain.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FSUIPC;
+using RestSharp;
+
+namespace realvaClient
+{
+    public partial class frmMain : Form
+    {
+
+        // =====================================
+        // DECLARE OFFSETS YOU WANT TO USE HERE
+        // =====================================
+        private Offset<uint> airspeed = new Offset<uint>(0x02BC);
+        private Offset<uint> avionicsMaster = new Offset<uint>(0x2E80);
+
+        private Offset<FsLongitude> playerLon = new Offset<FsLongitude>("LatLonPoint", 0x0568, 8);
+        private Offset<FsLatitude> playerLat = new Offset<FsLatitude>("LatLonPoint", 0x0560, 8);
+
+        private Boolean flightStarted = false;
+
+        private Double depLat;
+        private Double depLon;
+        private Double arrLat;
+        private Double arrLon;
+        private String startTime;
+        private String endTime;
+        private String aircraft;
+        private String savedCode;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            configureForm();
+            try
+            {
+                this.savedCode = File.ReadAllText("settings.txt");
+                this.txtSecretCode.Text = this.savedCode;
+            }
+            catch (FileNotFoundException e)
+            {
+
+            }
+            configureStartFlight();
+        }
+
+        // The connect/disconnect buton
+        private void btnToggleConnection_Click(object sender, EventArgs e)
+        {
+            if (FSUIPCConnection.IsOpen)
+            {
+                // Connection is currently open
+                // Stop the main timer
+                this.timerMain.Stop();
+                // Close the connection
+                FSUIPCConnection.Close();
+            }
+            else
+            {
+                // Try to open the connection
+                try
+                {
+                    this.lblConnectionStatus.Text = "Looking for a flight simulator...";
+                    this.lblConnectionStatus.ForeColor = Color.Goldenrod;
+                    FSUIPCConnection.Open();
+                    // If there was no problem, start the main timer
+                    this.timerMain.Start();
+                }
+                catch (Exception ex)
+                {
+                    // An error occured. Tell the user.
+                    MessageBox.Show("Connection Failed\n\n" + ex.Message, "FSUIPC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            configureForm();
+        }
+
+        // This method runs 20 times per second (every 50ms). This is set on the timerMain properties.
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            // Call process() to read/write data to/from FSUIPC
+            // We do this in a Try/Catch block incase something goes wrong
+            try
+            {
+                FSUIPCConnection.Process();
+                FSUIPCConnection.Process("LatLonPoint");
+
+
+                // Update the information on the form
+                // (See the Examples Application for more information on using Offsets).
+
+      
+              
+
+                FsLatLonPoint playerPos = new FsLatLonPoint(this.playerLat.Value, this.playerLon.Value);
+
+               // this.txtPlayerLocation.Text = this.playerLat.Value.DecimalDegrees.ToString();
+            }
+            catch (Exception ex)
+            {
+                // An error occured. Tell the user and stop this timer.
+                this.timerMain.Stop();
+                MessageBox.Show("Communication with FSUIPC Failed\n\n" + ex.Message, "FSUIPC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                // Update the connection status
+                configureForm();
+            }
+        }
+
+
+        // Configures the button and status label depending on if we're connected or not 
+        private void configureForm()
+        {
+            if (FSUIPCConnection.IsOpen)
+            {
+                this.btnToggleConnection.Text = "Disconnect";
+                this.lblConnectionStatus.Text = "Connected to " + FSUIPCConnection.FlightSimVersionConnected.ToString();
+                this.lblConnectionStatus.ForeColor = Color.Green;
+                this.btnFlight.Enabled = true;
+            }
+            else
+            {
+                this.btnToggleConnection.Text = "Connect";
+                this.lblConnectionStatus.Text = "Disconnected";
+                this.lblConnectionStatus.ForeColor = Color.Red;
+                this.btnFlight.Enabled = false;
+            }
+        }
+
+        private void configureStartFlight()
+        {
+            if (this.flightStarted)
+            {
+                this.btnFlight.Text = "Stop flight";
+                this.txtAircraft.Enabled = false;
+                this.txtSecretCode.Enabled = false;
+                File.WriteAllText("settings.txt", this.txtSecretCode.Text);
+            }
+            else
+            {
+                this.btnFlight.Text = "Start flight";
+                this.txtAircraft.Enabled = true;
+                this.txtSecretCode.Enabled = true;
+            }
+        }
+
+        // Form is closing so stop the timer and close FSUIPC Connection
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.timerMain.Stop();
+            FSUIPCConnection.Close();
+        }
+
+        private void btnFlight_Click(object sender, EventArgs e)
+        {
+
+            this.flightStarted = !this.flightStarted;
+
+            if(this.flightStarted)
+            {
+                this.depLat = this.playerLat.Value.DecimalDegrees;
+                this.depLon = this.playerLon.Value.DecimalDegrees;
+                this.startTime = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            else
+            {
+                this.arrLat = this.playerLat.Value.DecimalDegrees;
+                this.arrLon = this.playerLon.Value.DecimalDegrees;
+                this.endTime = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+
+                string createText = this.depLat + Environment.NewLine + this.depLon + Environment.NewLine + this.startTime + Environment.NewLine + this.arrLat + Environment.NewLine + this.arrLon + Environment.NewLine + this.endTime + Environment.NewLine;
+
+                var request = new RestRequest(Method.POST);
+                var client = new RestClient("https://realva-backend.herokuapp.com");
+                request.Resource = "/flight/submit";
+                request.RequestFormat = DataFormat.Json;
+                request.AddHeader("userSecretCode", txtSecretCode.Text);
+
+                request.AddJsonBody(
+                    new { 
+                        depLat,
+                        depLon,
+                        arrLat,
+                        arrLon,
+                        startTime,
+                        endTime,
+                        aircraft = txtAircraft.Text
+                });
+
+                client.Execute(request);
+
+                MessageBox.Show("Flight uploaded to server, you can start new flight / Let stavljen na sajt, mozete pokrenuti novi");
+            }
+
+            configureStartFlight();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSecretCode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
